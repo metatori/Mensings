c6bbc0b8fa82f35239ba9d47f9da71422106e442
metatori <khhok123@naver.com>
2025-05-28 18:23:39 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
-function goToSearch() {
-    window.location.href = "search.html";
+function goSearch() {
+  window.location.href = "search.html";
+}
+function goHome() {
+  window.location.href = "index.html";
+}
+function search() {
+  const keyword = document.getElementById("searchInput").value;
+  if (keyword.trim() !== "") {
+    window.location.href = "results.html";
+  }
 }
