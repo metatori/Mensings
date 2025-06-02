7af4a8b40798ba94ca999ddaa31a8bf9aba8a4a0
metatori <khhok123@naver.com>
2025-06-02 11:07:02 +0900

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-
-function searchProduct() {
-    const keyword = document.getElementById("searchInput").value;
-    console.log("검색 버튼이 눌렸습니다. 검색어:", keyword);
-    document.getElementById("result").innerText = "검색어: " + keyword + " (API 연동은 아직 테스트 중)";
-}
